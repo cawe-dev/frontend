128ef7f0c4144a8c97fe52aab530beaa7bef49f7
Cawe Alves <cawe.wewe04@gmail.com>
2026-01-12 16:01:31 -0300

    update(header): creating with nuxt ui header component

--- app/components/menu/HeaderMobile.vue
+++ app/components/menu/HeaderMobile.vue
@@ -1,38 +1,47 @@
 <script lang="ts" setup>
 const { context } = useContext()
 
-const pathData = computed(() => {
-  return context.value === 'professional'
-    ? 'M0,0 L1440,0 L1440,250 Q720,230 0,250 Z'
-    : 'M0,0 L1440,0 L1440,220 Q720,300 0,220 Z'
-})
 </script>
 
 <template>
-  <header class="relative py-24">
-    <div class="absolute inset-0">
-      <svg width="100%" height="100%" viewBox="0 0 1440 300" preserveAspectRatio="none">
-        <path :d="pathData" fill="currentColor" class="text-brand-secundary transition-all duration-1000 ease-in-out" />
-      </svg>
-    </div>
-
-    <div class="relative text-center">
-      <h1 class="font-bold text-4xl mb-8" :class="context === 'professional' ? 'text-white' : 'text-black'">
+  <UHeader :toggle="{ disabled: true, class: 'hidden' }" class="bg-brand-secundary">
+    <template #title>
+      <Logo class="h-6 w-auto" />
+      <h1 :class="context === 'professional' ? 'text-white' : 'text-black'">
         cawe.dev
       </h1>
+    </template>
 
-      <div class="inline-flex bg-black/10 p-1 rounded-full">
-        <button @click="context = 'professional'" class="px-8 py-2 rounded-full text-sm font-bold"
+    <UNavigationMenu />
+
+    <template #right>
+      <div class="inline-flex bg-black/10 p-1 mr-2 rounded-full">
+        <button @click="context = 'professional'" class="px-1 py-2 rounded-full text-sm font-bold"
           :class="context === 'professional' ? 'bg-brand-secundary text-white' : 'text-brand-text'">
           Profissional
         </button>
-        <button @click="context = 'personal'" class="px-8 py-2 rounded-full text-sm font-bold"
+        <button @click="context = 'personal'" class="px-1 py-2 rounded-full text-sm font-bold"
           :class="context === 'personal' ? 'bg-brand-secundary text-white' : 'text-brand-text'">
           Pessoal
         </button>
       </div>
-    </div>
-  </header>
+
+      <UTooltip text="News" :kbds="['meta', 'G']">
+        <UButton class="active:bg-brand-secundary" :class="context === 'professional' ? 'text-white' : 'text-black'"
+          variant="ghost" icon="i-mdi-light:bell" aria-label="GitHub" />
+      </UTooltip>
+
+      <UTooltip text="Open on GitHub" :kbds="['meta', 'G']">
+        <UButton class="active:bg-brand-secundary" :class="context === 'professional' ? 'text-white' : 'text-black'"
+          variant="ghost" to="https://github.com/cawe-dev" target="_blank" icon="i-simple-icons-github"
+          aria-label="GitHub" />
+      </UTooltip>
+    </template>
+
+    <template #bottom>
+    </template>
+
+  </UHeader>
 </template>
 
 <script setup>
