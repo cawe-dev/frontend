a9c166e32ba7ec295e1391071582d03d494b2d8c
Cawe Alves <cawe.wewe04@gmail.com>
2026-01-19 17:29:05 -0300

    feat(login): add pt-br language param to cognito login url

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -2,7 +2,7 @@
 const config = useRuntimeConfig()
 
 onMounted(async () => {
-  const cognitoUrl = `${config.public.cognitoDomain}/login?client_id=${config.public.cognitoClientId}&response_type=code&scope=email+openid+profile&redirect_uri=${config.public.cognitoRedirectUri}`
+  const cognitoUrl = `${config.public.cognitoDomain}/login?client_id=${config.public.cognitoClientId}&response_type=code&scope=email+openid+profile&redirect_uri=${config.public.cognitoRedirectUri}&&lang=pt-BR`
   window.location.href = cognitoUrl
 })
 </script>
